be1fb01ce2bde4066200382f89c6216319b02631
asepopov <asepopov@avito.ru>
2024-10-31 23:35:59 +0300

    add ssh script

---
+++ chat-worker-reboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CR_NAME="chat-worker"
+
+# Ищем все контейнеры, начинающиеся с этого префикса и заканчивающиеся числом
+for container in $(docker ps --filter "name=${CR_NAME}" --format "{{.Names}}"); do
+    # Проверяем, что имя контейнера заканчивается на число
+    if [[ $container =~ ^${CR_NAME}-[0-9]+$ ]]; then
+        echo "Перезапускаем контейнер: $container"
+        docker-compose -f ~/space21/staging/staging-deployment/docker-compose.yml \
+                    --env-file ~/space21/staging/.env up -d $container
+    else
+        echo "Пропускаем контейнер: $container"
+    fi
+done
